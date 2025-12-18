02b0a2b46645c0e3d524de52614bdc0659ca6627
Michael Christenson II <m3talsmith@gmail.com>
2025-12-18 16:10:21 -0500

    Initial polling implemented

---
+++ README.md
@@ -0,0 +1,13 @@
+# BubbleTea Polling
+
+This is an example of a BubbleTea application with polling
+
+## Development
+
+### Run
+
+To run the example type this in your terminal:
+
+```shell
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type tickMsg struct{}
+type finishedMsg struct{}
+
+type poller struct {
+	count int
+}
+
+func (p *poller) Init() tea.Cmd {
+	p.count = 1
+	return nil
+}
+
+func (p *poller) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return p, tea.Quit
+		}
+	case tickMsg:
+		p.count++
+		return p, poll(p)
+	case finishedMsg:
+		return p, tea.Quit
+	}
+
+	return p, poll(p)
+}
+
+func (p *poller) View() string {
+	return fmt.Sprintf("Count: %d", p.count)
+}
+
+func poll(p *poller) tea.Cmd {
+	if p.count < 10 {
+		return tea.Tick(1*time.Second, func(time.Time) tea.Msg {
+			return tickMsg{}
+		})
+	}
+	return tea.Tick(1*time.Second, func(time.Time) tea.Msg {
+		return finishedMsg{}
+	})
+}
+
+func main() {
+	tea.NewProgram(&poller{}).Run()
+}
